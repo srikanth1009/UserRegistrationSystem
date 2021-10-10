c25f746121992d644689f9ac12d19b7318ea05f0
srikanth1009 <srikanthreddyyaddala8@gmail.com>
2021-10-10 12:36:36 +0530

    Added:Enter Valid Mobile Number

--- src/com/bridgelabs/regex/UserRegistrationMain.java
+++ src/com/bridgelabs/regex/UserRegistrationMain.java
@@ -13,6 +13,7 @@ public class UserRegistrationMain {
         System.out.println("Enter 1 : to validate First Name ");
         System.out.println("Enter 2 : to validate Last Name ");
         System.out.println("Enter 3 : to validate E-mail Address ");
+        System.out.println("Enter 4 : to validate Mobile Number ");
         switch (scanner.nextInt()){
             case 1:
                 UserRegistration.validFirstName();
@@ -22,6 +23,9 @@ public class UserRegistrationMain {
                 break;
             case 3:
                 UserRegistration.validEmailId();
+                break;
+            case 4:
+                UserRegistration.validMobileNumber();
                 break;
             default:
                 System.out.println("Select a valid number");
